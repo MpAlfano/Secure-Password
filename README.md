842e7605b04d6959cb7517c4d30b26f7324866c2
Mark Alfano <mpalfano4@gmail.com>
2022-01-20 23:45:05 -0500

    tried re-writing and almost completed

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Secure-Password
+
+## Page to create a random password
+
+Asking a series of question to create a random password with decided upon
+numbers, letters being lowercase or uppercase and special characters.
+
+### Page
+
+This is an image preview of the webpage.
+
+![My Portfolio Page](./assets/images.demo.png)
+
+[View Website Here](https://mpalfano.github.io/Secure-Password/)
--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,200 +1,244 @@
 // Assignment Code
+// Global variables
 var generateBtn = document.querySelector("#generate");
+var passSize
+var passLower
+var passUpper
+var passNumber
+var passSpecial
+var Alpha = "abcdefghijklmnopqrstuvwxyz";
+var AlphaU = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var Num = "1234567890";
+var Special = "'!@#$%^&*()_+~`|}{[]\:;?><,./-='";
 
-var generateNumber
-var generateUpper
-var generateSize
-var generateLower
-var generateSpecial
 
-//Ask user for a number between 8 and 128 then validate the number
-//Ask user if they want to include each for lowercase, then uppercase, then numeric
-//and/or special characters. Then check to make sure at least 1 is selected
 
-var writePassword = function() {
-  
+var generatePassword = function(){
+  //Ask user for a number between 8 and 128 then validate the number
   var passSize = window.prompt("Enter a number between 8 and 128 for character length of password:");
-  
-  generateSize = passSize
-  console.log(generateSize);
-  
+
   if (!passSize) {
     return;
   } else if (isNaN(passSize)) {
-    writePassword();
-  } else if(passSize < 8 || passSize > 128) {
-    writePassword();
-  } else {
-    
-    writeLower();
+    return generatePassword()
+  } else if (passSize < 8 || passSize > 128) {
+    return generatePassword()
   }
-  
-}
 
-var writeLower = function() {
-  
-  var passLower = window.prompt("Do you want to include lowercase letters? (Y/N)", "Y/N");
+  var passLower = window.prompt("Type Y if you want lowercase letters, N if you don't. (Y/N)", "Y/N");
   passLower = passLower.toUpperCase();
 
-  generateLower = passLower
-  console.log(generateLower);
-
-  if (passLower === "Y") {
-    writeUpper();
-  } else if (passLower === "N") {
-    writeUpper();
+  if (!(passLower === "Y" )) {
+    passLower = "Y"
+    console.log(passLower)
   } else {
-    writeLower();
+    passLower = "N";   
+  } 
+  console.log(passLower)
+
+  var passLower = window.prompt("Type Y if you want uppercase letters, N if you don't. (Y/N)", "Y/N");
+  passLower = passLower.toUpperCase();
+
+  if (!(passUpper === "Y" )) {
+    passUpper = "Y";
+    console.log(passUpper)
+  } else {
+      passUpper = "N";
   }
-} 
+  console.log(passUpper)
 
-var writeUpper = function() {
-  
-  var passUpper = window.prompt("Do you want to include uppercase letters? (Y/N)", "Y/N")
-  passUpper = passUpper.toUpperCase()
-  
-  generateUpper = passUpper
-  console.log(generateUpper);
-
-  if (passUpper === "Y") {
-    writeNumber();
-  } else if (passUpper === "N") {
-    writeNumber();
-  }else {
-    writeUpper();
-    console.log(generateUpper);
-  }
-} 
-
-var writeNumber = function() {
-  
-  var passNumber = window.prompt("Do you want to include numbers? (Y/N)", "Y/N")
+  var passNumber = window.prompt("Type Y if you want to include numbers, N if you don't. (Y/N)", "Y/N")
   passNumber = passNumber.toUpperCase();
   
-  generateNumber = passNumber
-  console.log(generateNumber);
-
-  if (passNumber === "Y") {
-    writeSpecial();
-  } else if (passNumber === "N") {
-    writeSpecial();
-  }else {
-    writeNumber()
+  if (!(passNumber === "Y" )) {
+    passNumber = "Y";
+    console.log(passNumber)
+  } else {
+      passNumber = "N";
   }
-  
-} 
+  console.log(passNumber)
 
-var writeSpecial = function() {
-  
-  var passSpecial = window.prompt("Do you want to include special characters? (Y/N)", "Y/N")
+  var passSpecial = window.prompt("Type Y if you want to include special characters, N if you don't. (Y/N)", "Y/N")
   passSpecial = passSpecial.toUpperCase();
 
-  generateSpecial = passSpecial
-  console.log(generateSpecial);
+  if (!(passSpecial !== "Y" )) {
+    passSpecial = "Y";
+    console.log(passSpecial)
+  } else {
+    passSpecial = "N";
+  }
+  console.log(passSpecial)
 
-  console.log(generateSize);
-  console.log(generateLower);
-  console.log(generateUpper);
-  console.log(generateNumber);
-  console.log(generateSpecial);
-
-  if (passSpecial === "Y") {
-    createPassword();
-  } else if (passSpecial === "N") {
-    createPassword();
-  }else {
-    writeSpecial();
-  } 
-} 
-
-var keyListAlpha = "abcdefghijklmnopqrstuvwxyz";
-var keyListAlphaU = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var keyListNum = "1234567890";
-var keyListSpec = "'!@#$%^&*()_+~`|}{[]\:;?><,./-='";
-
-
-
-var createPassword = function() {
-
-  console.log(generateSize);
-  console.log(generateLower);
-  console.log(generateUpper);
-  console.log(generateNumber);
-  console.log(generateSpecial);
-
-  var createLogic = generateLower + generateUpper + generateNumber + generateSpecial;
-
+  var createLogic = passLower + passUpper + passNumber + passSpecial;
+  //Check length is correct
   console.log(createLogic)
 
-  if (createLogic = "YYYY"){
-    createPassword1 
-  } else if (createLogic = "YNYY") {
-    createPassword2
-  } else if (createLogic = "YYNY") {
-    createPassword3
-  } else if (createLogic = "YYYN") {
-    createPassword4
-  } else if (createLogic = "NYYY") {
-    createPassword5
-  } else if (createLogic = "NYYN") {
-    createPassword6
-  } else if (createLogic = "YNNY") {
-    createPassword7
-  } else if (createLogic = "YNYN") {
-    createPassword8
-  } else if (createLogic = "NNYY") {
-    createPassword9
-  } else if (createLogic = "NYNY") {
-    createPassword10
-  } else if (createLogic = "YYNN") {
-    createPassword11
-  } else if (createLogic = "YNNN") {
-    createPassword12
-  } else if (createLogic = "NYNN") {
-    createPassword13
-  } else if (createLogic = "NNYN") {
-    createPassword14
-  } else if (createLogic = "NNNY") {
-    createPassword15
-  } else if (createLogic = "NNNN") {
-    createPassword16
-  }
+  if (createLogic === "YYYY") {
+    var length = passSize,
+    charset = Alpha + AlphaU + Num + Special,
+    passVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+      passVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    console.log(passVal)
+    return;
+  } else if (createLogic === "YNYY") {
+    var length = passSize,
+    charset = Alpha + Num + Special,
+    passVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+      passVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    console.log(passVal)
+    return;
+  } else if (createLogic === "YYYN") {
+    var length = passSize,
+    charset = Alpha + AlphaU + Special,
+    passVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+      passVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    console.log(passVal)
+    return;
+  } else if (createLogic === "YYYN") {
+    var length = passSize,
+    charset = Alpha + AlphaU + Num,
+    passVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+      passVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    console.log(passVal)
+    return;
+  } else if (createLogic === "YYYY") {
+    var length = passSize,
+    charset = Alpha + AlphaU + Num + Special,
+    passVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+      passVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    console.log(passVal)
+    return;
+  } else if (createLogic === "NYYY") {
+    var length = passSize,
+    charset = AlphaU + Num + Special,
+    passVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+      passVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    console.log(passVal)
+    return;
+  } else if (createLogic === "NYYN") {
+    var length = passSize,
+    charset = AlphaU + Num,
+    passVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+      passVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    console.log(passVal)
+    return;
+  } else if (createLogic === "YNNY") {
+    var length = passSize,
+    charset = Alpha + Special,
+    passVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+      passVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    console.log(passVal)
+    return;
+  } else if (createLogic === "YNYN") {
+    var length = passSize,
+    charset = Alpha + Num,
+    passVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+      passVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    console.log(passVal)
+    return;
+  } else if (createLogic === "NNYY") {
+    var length = passSize,
+    charset = Num + Special,
+    passVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+      passVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    console.log(passVal)
+    return;
+  } else if (createLogic === "YYYY") {
+    var length = passSize,
+    charset = Alpha + AlphaU + Num + Special,
+    passVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+      passVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    console.log(passVal)
+    return;
+  } else if (createLogic === "NYNY") {
+    var length = passSize,
+    charset = AlphaU + Special,
+    passVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+      passVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    console.log(passVal)
+    return;
+  } else if (createLogic === "YYNN") {
+    var length = passSize,
+    charset = Alpha + AlphaU,
+    passVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+      passVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    console.log(passVal)
+    return;
+  } else if (createLogic === "YNNN") {
+    var length = passSize,
+    charset = Alpha,
+    passVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+      passVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    console.log(passVal)
+    return;
+  } else if (createLogic === "NYNN") {
+    var length = passSize,
+    charset = AlphaU,
+    passVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+      passVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    console.log(passVal)
+    return;
+  } else if (createLogic === "NNYN") {
+    var length = passSize,
+    charset = Num,
+    passVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+      passVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    console.log(passVal)
+    return;
+  } else if (createLogic === "NNNY") {
+    var length = passSize,
+    charset = Special,
+    passVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+      passVal += charset.charAt(Math.floor(Math.random() * n));
+      var passwordText = document.querySelector("#password");
+      passwordText.value = passVal;
+    }
+ } 
+ var passwordText = document.querySelector("#password");
+  passwordText.value = passVal;
 
-
-
-  /*var len = Math.ceil(generateSize/2);
-  len = len - 1;
-  var lenSpec = generateSize-2*len;
-
-  for (i=0;i<len;i++) {
-    password1+=keyListAlpha.charAt(Math.floor(Math.random()*keyListAlpha.length));
-    password1+=keyListNum.charAt(Math.floor(Math.random()*keyListNum.length));
-  }
-
-  for (i=0;i<lenSpec;i++)
-        password1+=keyListSpec.charAt(Math.floor(Math.random()*keyListSpec.length));
-        password1+=keyListAlphaU.charAt(Math.floor(Math.random()*keyListAlphaU.length));
-
-    password1=password1.split('').sort(function(){return 0.5-Math.random()}).join('');
-    
-    console.log(password1)
-    console.log(password1.length)
-    return password1;*/
 }
 
-
-
+/* Assignment Code
+var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
-  var password = createPassword();
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
-}
-
-//function that takes options to generate random password
-
-
+} */
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", generatePassword);
